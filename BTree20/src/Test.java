ed86cbe82da419aba2927e774b26071ab4b73a8c
Jamie <Jamie@Jxtreme>
2014-03-24 17:16:32 -0400

    goodn for you Jamie

--- BTree20/src/Test.java
+++ BTree20/src/Test.java
@@ -1,9 +1,13 @@
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 public class Test {
 
 	public static void main(String[] args) {
 		//JsoupTestStringManipulating();
-		testAdd();
+		//testAdd();
+		//testSave();
 
 	}
 	
@@ -37,5 +41,29 @@ public class Test {
 		System.out.println("right: "+tree.getRoot().links.get(3).keys);
 
 	}
+	
+	public static void testSave(){
+		BTree tree = new BTree();	
+		tree.insert("apple"); 
+		tree.insert("sand");
+		tree.insert("math");
+		tree.insert("tree");
+		tree.insert("north");
+		tree.insert("onion");
+		tree.insert("pan");
+		tree.insert("pink");
+		tree.insert("pool"); 
+		File myFile = new File("C:\\BTree.ser");
+		try{
+			FileOutputStream fout = new FileOutputStream(myFile);
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			oos.writeObject(tree);
+			oos.close();
+		}
+		catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}// end save test
+	
 	
 }//end class 
